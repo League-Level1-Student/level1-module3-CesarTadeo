21b7d75f3f709963ebe85a2ff38b620ee5a2f7d4
CesarTadeo <43711012+CesarTadeo@users.noreply.github.com>
2021-03-29 17:09:03 -0700

    flappybird

--- src/_01_intro_to_static/Athlete.java
+++ src/_01_intro_to_static/Athlete.java
@@ -3,6 +3,7 @@ package _01_intro_to_static;
 public class Athlete {
     static int nextBibNumber;
     static String raceLocation = "New York";
+    static String raceStartTime = "9.00am";
 
     String name;
     int speed;
